104371376dfa725277ecbdb47588605159719342
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-08-30 10:05:25 -0700

    Implemented prompts 1 and 2

--- src/recursion.js
+++ src/recursion.js
@@ -7,11 +7,23 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) return null;
+  if (n === 0) return 1;
+  if (n === 1) return 1;
+
+  return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  var arrayCopy = array.slice();
+  var total = 0;
+
+  if (arrayCopy.length === 0) return 0;
+  total += arrayCopy.pop(array.length - 1)
+
+  return total += sum(arrayCopy);
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
